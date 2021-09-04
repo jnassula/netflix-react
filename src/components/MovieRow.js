8ed003edef548aa76c700fd51bc640ef9146dc55
jnassula <jonatanassula@gmail.com>
2021-09-04 16:09:07 +0100

    developed navigate arrow

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './MovieRow.css'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default ({ title, items }) => {
+
+    const handleLeftArrow = () => {
+
+    }
+
+    const handleRighttArrow = () => {
+        
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize: 50}} />
+            </div>
+            <div className="movieRow--right" onClick={handleRighttArrow}>
+                <NavigateNextIcon style={{fontSize: 50}} />
+            </div>
             <div className="movieRow--listarea">
                 <div className="movieRow--list">
                     {items.results.length > 0 && items.results.map((item, key) => (
